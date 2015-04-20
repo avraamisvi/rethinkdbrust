7b8315210d786f95be63bb4b5f5822e0bd95262c
Paulo Suzart <paulosuzart@gmail.com>
2015-04-20 19:24:36 -0300

    Fied module exports

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -29,9 +29,10 @@ let mut rethinkdb = RethinkDB::connect("localhost", 7888, "AUTH", 3);
 db("test").table_create("person_create").replicas(1i32).run(&mut rethinkdb);
 ```
 */	
-	pub use core::*;
+	
 	pub use core::select::*;
 	pub use core::table::*;
+	pub use core::write::*;
 }
 
 
